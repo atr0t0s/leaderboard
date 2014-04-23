53b956fc34866a2964d6d82827c58837bb5727ca
George Violaris <violarisgeorge@gmail.com>
2014-04-23 12:51:30 +0300

    Split user control functions to different file

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -57,95 +57,6 @@ func (c App) Index() revel.Result {
 // Parameters:
 // dbname -> the mongodb database name, collection -> the mongodb collection
 // -----------
-func (c App) CreateUser(dbname, collection, user, email, pass string) revel.Result {
-
-	// connect to DB server(s)
-
-	if uri == "" {
-		fmt.Println("no connection string provided")
-		os.Exit(1)
-	}
-	session, err := mgo.Dial(uri)
-	if err != nil {
-		panic(err)
-	}
-
-	defer session.Close()
-
-	// select DB and Collection
-	d := session.DB(dbname).C(collection)
-
-	// TODO: Use encryption through crypto package to hash passwords
-
-	// Query to see if user already exists in collection
-	var doc User
-	var results []User
-	err = d.Find(bson.M{"username": user}).Sort("-timestamp").All(&results)
-
-	if err != nil {
-		panic(err)
-	} else {
-		if len(results) == 0 {
-			//do DB operations
-			doc = User{Id: bson.NewObjectId(), Username: user, Email: email, Password: pass}
-			err = d.Insert(doc)
-			if err != nil {
-				panic(err)
-			}
-		} else {
-			return c.RenderJson("Error: User already exists")
-		}
-	}
-
-	return c.RenderJson(doc)
-
-}
-
-func (c App) Auth(dbname, collection, user, pass string) revel.Result {
-
-	// connect to DB server(s)
-
-	if uri == "" {
-		fmt.Println("no connection string provided")
-		os.Exit(1)
-	}
-	session, err := mgo.Dial(uri)
-	if err != nil {
-		panic(err)
-	}
-
-	defer session.Close()
-
-	// select DB and Collection
-	d := session.DB(dbname).C(collection)
-
-	// TODO: Use encryption through crypto package to hash passwords
-
-	// Query to authenticate
-
-	var results []User
-
-	err = d.Find(bson.M{"username": user, "password": pass}).Sort("-timestamp").All(&results)
-
-	if err != nil {
-		panic(err)
-	} else {
-		//fmt.Println(err)
-		c.Session["user"] = user
-		c.Flash.Success("Welcome, " + user)
-	}
-
-	return c.RenderJson(results)
-}
-
-func (c App) Logout() revel.Result {
-
-	c.Session["user"] = ""
-
-	session := c.Session["user"]
-
-	return c.RenderJson(session)
-}
 
 func (c App) CreateStat(dbname, collection, statName, statMetric string) revel.Result {
 
@@ -185,14 +96,6 @@ func (c App) CreateStat(dbname, collection, statName, statMetric string) revel.R
 	}
 
 	return c.RenderJson(doc)
-}
-
-func (c App) SaveUserStat(statName, username string) revel.Result {
-
-}
-
-func (c App) GetUserStats(username string) {
-
 }
 
 func (c App) DefineAchievement(achName, statName string, minVal int) {
