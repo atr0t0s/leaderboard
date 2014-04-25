664e0ec992121095cc2a94b805a77befdb639cc1
George Violaris <violarisgeorge@gmail.com>
2014-04-26 01:49:20 +0300

    Working on adding bcrypt for user creation and authentication

--- app/controllers/users.go
+++ app/controllers/users.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"code.google.com/p/go.crypto/bcrypt"
+	//"encoding/base64"
+	"fmt"
 	"github.com/revel/revel"
 	"labix.org/v2/mgo/bson"
 	"leaderboard/app/models"
@@ -8,60 +11,68 @@ import (
 
 // Create Users via HTTP POST call to /App/CreateUser
 // You can manually add/remove fields by changing the params and 'doc' variable
-func (c App) CreateUser(name, user, email, pass, role string) revel.Result {
+func (c App) CreateUser(name string, user string, email string, pass []byte, role string) revel.Result {
 
-	if c.Session["user"] == "" || c.Session["role"] != adminrole {
+	/*if c.Session["user"] == "" || c.Session["role"] != adminrole {
 		return c.RenderJson("Sorry, only the cluster admin can perform that action")
+	} else {*/
+	// connect to DB server
+	d, s := db(usercol)
+
+	// Query to see if user already exists in collection
+	var doc models.User
+	var results []models.User
+	doc.HashPass, _ = bcrypt.GenerateFromPassword(
+		pass, bcrypt.DefaultCost)
+
+	err := d.Find(bson.M{"username": user}).Sort("-timestamp").All(&results)
+	fmt.Println(pass)
+	if err != nil {
+		panic(err)
 	} else {
-		// connect to DB server
-		d, s := db(usercol)
-
-		// Query to see if user already exists in collection
-		var doc models.User
-		var results []models.User
-
-		err := d.Find(bson.M{"username": user}).Sort("-timestamp").All(&results)
-
-		if err != nil {
-			panic(err)
-		} else {
-			if len(results) == 0 {
-				//do DB operations
-				doc = models.User{Id: bson.NewObjectId(), Name: name, Username: user, Email: email, Password: pass}
-				err = d.Insert(doc)
-				if err != nil {
-					panic(err)
-				}
-			} else {
-				return c.RenderJson("Error: User already exists")
+		if len(results) == 0 {
+			//do DB operations
+			doc = models.User{Id: bson.NewObjectId(), Name: name, Username: user, Email: email, HashPass: doc.HashPass, Role: role}
+			err = d.Insert(doc)
+			if err != nil {
+				panic(err)
 			}
+		} else {
+			return c.RenderJson("Error: User already exists")
 		}
-
-		s.Close()
-
-		return c.RenderJson(doc)
 	}
+
+	s.Close()
+
+	return c.RenderJson(doc)
+	//}
 }
 
-func (c App) Auth(user, pass string) revel.Result {
+func (c App) Auth(user string, pass []byte) revel.Result {
 
 	// connect to DB server(s)
 	d, s := db(usercol)
 
-	// TODO: Use encryption through crypto package to hash passwords
 	// Query to authenticate
 
 	results := models.User{}
-	query := bson.M{"username": user, "password": pass}
+	query := bson.M{"username": user}
 	err := d.Find(query).One(&results)
+
+	// TODO: fix this, there's something seriously wrong with getting the
+	// pass []byte from the argument, it comes out as null, i.e. []
+	verify := bcrypt.CompareHashAndPassword(results.HashPass, pass)
 
 	if err != nil {
 		panic(err)
 	} else {
-		//fmt.Println(err)
-		c.Session["user"] = results.Username
-		c.Session["role"] = results.Role
-		c.Flash.Success("Welcome, " + results.Username)
+		if verify != nil {
+			panic(verify)
+		} else {
+			c.Session["user"] = results.Username
+			c.Session["role"] = results.Role
+			c.Flash.Success("Welcome, " + results.Username)
+		}
 	}
 
 	s.Close()
